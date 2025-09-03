fcbe1fbe64d039c2a3be7b0873852185030df48a
omanytea <omanytea@school21.ru>
2025-09-03 15:29:23 +0300

    create model.h model.cpp and ddelete obj_loader.cpp

--- src/model/model.cpp
+++ src/model/model.cpp
@@ -1,9 +1,66 @@
 #include "model.h"
 
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
 namespace s21
 {
-    int sum(int a, int b)
+    const std::vector<Vertex> &Model::getVertices() const
     {
-        return a + b;
+        return vertices_;
+    }
+
+    const std::vector<Faces> &Model::getFaces() const
+    {
+        return faces_;
+    }
+    s21::Model Model::loadFromFile(const std::string &filePath)
+    {
+        s21::Model model;
+        std::ifstream inputFile(filePath);
+
+        if (!inputFile.is_open())
+        {
+            std::cerr << "Не удалось открыть файл!\n"
+                      << filePath << std::endl;
+            return model;
+        }
+
+        std::string line;
+
+        while (std::getline(inputFile, line))
+        {
+            if (line.empty())
+            {
+                continue;
+            }
+
+            std::stringstream ss(line);
+            std::string type;
+            ss >> type;
+
+            if (type == "v")
+            {
+                Vertex v;
+                ss >> v.x >> v.y >> v.z;
+                model.vertices_.push_back(v);
+            }
+            if (type == "f")
+            {
+                Faces f;
+                int vertex_index;
+                while (ss >> vertex_index)
+                {
+                    f.vertices.push_back(vertex_index - 1);
+                }
+                model.faces_.push_back(f);
+            }
+        }
+        std::cout << "Модель загружена: " << filePath << std::endl;
+        std::cout << "Вершин: " << model.getVertices().size() << std::endl;
+        std::cout << "Полигонов: " << model.getFaces().size() << std::endl;
+
+        return model;
     }
 }
--- src/model/model.h
+++ src/model/model.h
@@ -1,9 +1,38 @@
+#include <vector>
+#include <string>
 #ifndef MODEL_H_
 #define MODEL_H_
 
 namespace s21
 {
-    int sum(int a, int b);
+
+    struct Vertex
+    {
+        double x;
+        double y;
+        double z;
+    };
+
+    struct Faces
+    {
+        std::vector<int> vertices;
+    };
+
+    class Model
+    {
+    public:
+        Model() = default;
+        Model(const std::vector<Vertex> &vertices, const std::vector<Faces> &faces);
+
+        const std::vector<Vertex> &getVertices() const;
+        const std::vector<Faces> &getFaces() const;
+
+        static Model loadFromFile(const std::string &filePath);
+
+    private:
+        std::vector<Vertex> vertices_;
+        std::vector<Faces> faces_;
+    };
 }
 
 #endif
